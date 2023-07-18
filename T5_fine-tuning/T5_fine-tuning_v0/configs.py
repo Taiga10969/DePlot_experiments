a54cddca2604c053390300bed9a2fc1b1461bd8f
Taiga MASUDA <106218669+Taiga10969@users.noreply.github.com>
2023-07-18 16:02:16 +0900

    Create configs.py

---
+++ T5_fine-tuning/T5_fine-tuning_v0/configs.py
@@ -0,0 +1,21 @@
+class Config(object):
+    def __init__(self):
+        
+        # パスの設定
+        self.dataset_TRAIN_csv_file_path = '/taiga/Datasets/cv_ml_figures/deplot_text_data_train.csv'
+        self.dataset_TEST_csv_file_path = '/taiga/Datasets/cv_ml_figures/deplot_text_data_test.csv'
+
+        self.dataset_dir_path = '/taiga/Datasets/cv_ml_figures/'
+
+
+        self.record_dir = '/taiga/experiment/T5/record'
+
+        
+        self.max_source_length = 512  #sourceの入力文字列の最大列数
+        self.max_target_length = 128  #targetの入力文字列の最大列数
+
+        self.batch_size = 128  #バッチサイズ
+        self.lr = 5e-4  #学習率
+        self.weight_decay = 0.1
+        self.t0 = 15
+        self.epoch = 100
