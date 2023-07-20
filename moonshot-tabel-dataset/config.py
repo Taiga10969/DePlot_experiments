0ab6be9f3ed32178998c764c81656121ddb34bc0
Taiga MASUDA <106218669+Taiga10969@users.noreply.github.com>
2023-07-20 14:41:12 +0900

    Create config.py

---
+++ moonshot-tabel-dataset/config.py
@@ -0,0 +1,6 @@
+class Config():
+    def __init__(self):
+
+        self.dataset_dir_pth = '/taiga/Datasets/moonshot-dataset'
+        self.output_file_name = 'moonshot-table_dataset.csv' # 保存されるcsvファイル名
+        self.batch_size = 1
